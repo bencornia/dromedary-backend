b870e001d78923d2858fa20b2685ffe115c968b7
bencornia <ben.cornia@gmail.com>
2023-03-28 05:09:50 -0600

    Update user controller

--- models/users.model.js
+++ models/users.model.js
@@ -1,17 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    businessName: String,
-    ownerName: String,
-    email: String,
-    password: String,
-    apiKey: String,
-    profileImagePath: String,
-    createdDate: String,
-    lastUpdatedDate: String,
+	businessName: String,
+	ownerName: String,
+	email: String,
+	password: String,
+	profileImagePath: String,
+	createdDate: String,
+	lastUpdatedDate: String,
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
